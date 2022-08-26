4cd7d96079c57a8307c4e2a93454b9f6a3cd6129
NickLegacy <nicklegacy13@gmail.com>
2022-08-25 22:12:22 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 17;
+            int B = 4;
+            int quotient = A / B;
+            int remainder = A % B;
+
+
+            Console.WriteLine($"{A} / {B} is {quotient} remainder {remainder}");
+
+            {
+
+                Console.WriteLine("What is the radius of your circle?");
+
+                var R = double.Parse(Console.ReadLine());
+                var AreaOfCircle = Math.PI * R * R;
+
+                Console.WriteLine(value: $"The area of a circle with radius of {R} is {AreaOfCircle}");
+            }
+
+
+
+            }
+    }
+}
